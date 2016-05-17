707674d9084db3138f8b89c3f57d22fd7c921f92
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2016-05-17 22:54:39 +0545

    angular js added

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ var devb = angular.module('devb', ['ngRoute']);
 devb.config(['$routeProvider',
     function($routeProvider) {
         $routeProvider.
-        when('/', {
-            templateUrl: 'index.html'
-        }).
         when('/portfolio', {
             templateUrl: 'portfolio.html'
         }).
